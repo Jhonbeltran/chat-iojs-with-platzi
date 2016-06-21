81bde5f854e59a82fa8baa1fcf362a13130daf4d
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-21 18:25:30 -0500

    Agrego callback en el request handler y en el listen. Aquí agregamos un par de callbacks a nuestro servidor, el primero es una funcion anonima que se ejecutará cada vez que llegue una peticion http a nuestro servidor, el segundo se ejecutará cuando el servidor inicie por primera vez. Recuerda, un callback es una funcion js que se ejecuta cuando una operacion asincrona ha terminado.

--- server.js
+++ server.js
@@ -14,10 +14,29 @@ const port = process.env.PORT || 8080
 
 
 //Con esto creamos el server 
-const server = http.createServer()
+// const server = http.createServer()
 
 //El puerto que va a escuchar nuestra aplicacion
-server.listen(port)
+// server.listen(port)
 
 //Desde consola podemos usar PORT=8081 por ejemplo para cambiar la ruta del puerto
 
+/*Callbacks: funciones que se ejecutan despues de que algun 
+metodo asincrono (node es una plataforma asincrona / no bloquea el io (imput/output))
+a terminado su ejecucion. cuando hacemos una operacion en node, esta operacion 
+va a una cola de operaciones (eventlopp: ciclo que corre en el procesador constantemente
+verificando que tareas hay por ejecutar, cuando las tareas se ejecutan el va a notificar 
+un evento al evento que esta pidiendo que se llame esa tarea)*/
+
+/*Los callbacks nos permiten tener aplicaciones que puedan tener alta concurrencia,
+ aplicaciones en tiempo real*/
+
+//vamos a crear un callback que se va a ejecutar cada vez que se hace una peticion http
+const server = http.createServer(/*Hey miren, una anemona anonima*/function (req, res) {
+	res.end('Hola io.js que ahora es igual a node.js')
+})
+
+
+server.listen(port, /*se ejecuta cuando el server termina de escuchar>*/function () {
+	console.log('El servidor está escuchando en el puerto: ' + port)
+})
