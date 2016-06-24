0e9607f6024e243a7d36596ceea7fe0dc19be549
Jhonbeltran <jbeltranleon@gmail.com>
2016-06-23 20:37:24 -0500

    broken: intento agregar cosas al html pero no las carga

--- public/app.js
+++ public/app.js
@@ -5,6 +5,33 @@ function inicio(){
 
 function calcular () {
 	let entrada = document.getElementById('entrada')
+	entrada = entrada.value
 	let salida = document.getElementById('salida')
-	alert('Esto funciona')
+	
+	let senx, cosx, tanx, cotx, secx, cscx
+
+	senx = ["1/cscx", "tanx*cosx", "cosx/cotx"]
+	cosx = ["1/secx", "senx/tanx", "senx*cotx"]
+	tanx = ["1/cotx", "senx/cosx"]
+	cotx = ["1/tanx", "cosx/senx"]
+	//Acá podria usar variables para secx y para cscx pero neh
+	secx = ["1/cosx"]
+	cscx = ["1/senx"]
+
+	if (entrada=="senx" || entrada=="sen" || entrada=="sinx" || entrada=="sin") {
+		salida.innerHTML = "<ul><li>"+senx[0]+"</li>"+
+							"<li>"+senx[1]+"</li>"+
+							"<li>"+senx[2]+"</li>"+"</ul>"
+	}
+
+	if (entrada=="cosx" || entrada=="cos") {
+		salida.innerHTML = "<ul><li>"+cosx[0]+"</li>"+
+							"<li>"+cosx[1]+"</li>"+
+							"<li>"+cosx[2]+"</li>"+"</ul>"
+	}
+
+	
+
+
+
 }
